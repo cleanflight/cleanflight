50cca9feb5551e2901826c156e655255b6dcde15
Pierre Hugo <avoid3d@gmail.com>
2015-01-23 15:46:45 -0800

    Added some maths tests (will move into own module soon).

--- src/test/unit/flight_imu_unittest.cc
+++ src/test/unit/flight_imu_unittest.cc
@@ -24,6 +24,7 @@
 
 extern "C" {
     #include "common/axis.h"
+    #include "common/maths.h"
     #include "flight/flight.h"
 
     #include "sensors/sensors.h"
@@ -47,11 +48,127 @@ extern "C" {
 #define DOWNWARDS_THRUST true
 #define UPWARDS_THRUST false
 
+void testConstrain(void)
+{
+    // Within bounds.
+    EXPECT_EQ(constrain(1, 0, 2), 1);
+
+    // Equal to bottom bound.
+    EXPECT_EQ(constrain(1, 1, 2), 1);
+    // Equal to top bound.
+    EXPECT_EQ(constrain(1, 0, 1), 1);
+
+    // Equal to both bottom and top bound.
+    EXPECT_EQ(constrain(1, 1, 1), 1);
+
+    // Above top bound.
+    EXPECT_EQ(constrain(2, 0, 1), 1);
+    // Below bottom bound.
+    EXPECT_EQ(constrain(0, 1, 2), 1);
+
+    // Above bouth bounds.
+    EXPECT_EQ(constrain(2, 0, 1), 1);
+    // Below bouth bounds.
+    EXPECT_EQ(constrain(0, 1, 2), 1);
+}
+
+void testDegreesToRadians(void)
+{
+    EXPECT_EQ(degreesToRadians(0), 0.0f);
+    EXPECT_EQ(degreesToRadians(90), 0.5f * M_PIf);
+    EXPECT_EQ(degreesToRadians(180), M_PIf);
+    EXPECT_EQ(degreesToRadians(-180), - M_PIf);
+}
+
+void testConstrainf(void)
+{
+    // Within bounds.
+    EXPECT_EQ(constrainf(1.0f, 0.0f, 2.0f), 1.0f);
+
+    // Equal to bottom bound.
+    EXPECT_EQ(constrainf(1.0f, 1.0f, 2.0f), 1.0f);
+    // Equal to top bound.
+    EXPECT_EQ(constrainf(1.0f, 0.0f, 1.0f), 1.0f);
+
+    // Equal to both bottom and top bound.
+    EXPECT_EQ(constrainf(1.0f, 1.0f, 1.0f), 1.0f);
+
+    // Above top bound.
+    EXPECT_EQ(constrainf(2.0f, 0.0f, 1.0f), 1.0f);
+    // Below bottom bound.
+    EXPECT_EQ(constrainf(0, 1.0f, 2.0f), 1.0f);
+
+    // Above bouth bounds.
+    EXPECT_EQ(constrainf(2.0f, 0.0f, 1.0f), 1.0f);
+    // Below bouth bounds.
+    EXPECT_EQ(constrainf(0, 1.0f, 2.0f), 1.0f);
+}
+
+void testApplyDeadband(void)
+{
+    EXPECT_EQ(applyDeadband(0, 0), 0);
+    EXPECT_EQ(applyDeadband(1, 0), 1);
+    EXPECT_EQ(applyDeadband(-1, 0), -1);
+
+    EXPECT_EQ(applyDeadband(0, 10), 0);
+    EXPECT_EQ(applyDeadband(1, 10), 0);
+    EXPECT_EQ(applyDeadband(10, 10), 0);
+
+    EXPECT_EQ(applyDeadband(11, 10), 1);
+    EXPECT_EQ(applyDeadband(-11, 10), -1);
+}
+
+void expectVectorsAreEqual(struct fp_vector *a, struct fp_vector *b)
+{
+    EXPECT_EQ(a->X, b->X);
+    EXPECT_EQ(a->Y, b->Y);
+    EXPECT_EQ(a->Z, b->Z);
+}
+
+void testRotateVNoAngle()
+{
+    fp_vector vector = {1.0f, 0.0f, 0.0f};
+    fp_angles_t euler_angles = {.raw={0.0f, 0.0f, 0.0f}};
+
+    rotateV(&vector, &euler_angles);
+    fp_vector expected_result = {1.0f, 0.0f, 0.0f};
+
+    expectVectorsAreEqual(&vector, &expected_result);
+}
+
+void testRotateVAroundAxis()
+{
+    // Each time we rotate a vector <1, 0, 0> around an axis.
+    // Rotating 90 degrees around the X axis should do nothing.
+    fp_vector vector = {1.0f, 0.0f, 0.0f};
+    fp_angles_t euler_angles = {.raw={90.0f, 0.0f, 0.0f}};
+
+    rotateV(&vector, &euler_angles);
+    fp_vector expected_result = {1.0f, 0.0f, 0.0f};
+
+    expectVectorsAreEqual(&vector, &expected_result);
+}
+
+void testRotateV()
+{
+    testRotateVNoAngle();
+    testRotateVAroundAxis();
+}
+
+void testMaths(void)
+{
+    testConstrain();
+    testConstrainf();
+    testApplyDeadband();
+    testDegreesToRadians();
+    testRotateV();
+}
 
 TEST(FlightImuTest, Placeholder)
 {
     // TODO test things
     EXPECT_EQ(true, true);
+    testMaths();
 }
 
 // STUBS
