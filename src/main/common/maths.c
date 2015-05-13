8233c29485eb2f2eda9ba0ebb35c015a7543bf44
Mauro Mombelli <mauro@mombelli.me>
2015-05-13 10:58:06 +0200

    consistency fix

--- src/main/common/maths.c
+++ src/main/common/maths.c
@@ -141,10 +141,6 @@ void rotateV(struct fp_vector *v, fp_angles_t *delta)
     v->Z = v_tmp.X * mat[0][2] + v_tmp.Y * mat[1][2] + v_tmp.Z * mat[2][2];
 }
 
-int16_t map(int16_t x, int16_t in_min, int16_t in_max, int16_t out_min, int16_t out_max){
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-float mapf(float x, float in_min, float in_max, float out_min, float out_max){
+float scaleRangef(float x, float in_min, float in_max, float out_min, float out_max){
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
