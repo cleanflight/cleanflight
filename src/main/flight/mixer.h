ee9ebacf03434e4aec39c4a281d746030a44031d
Mauro Mombelli <mauro@mombelli.me>
2015-05-18 15:06:31 +0200

    fixed thrust compensation AND rollYaw math

--- src/main/flight/mixer.h
+++ src/main/flight/mixer.h
@@ -132,7 +132,7 @@ void mixerUseConfigs(
 void writeAllMotors(int16_t mc);
 void mixerLoadMix(int index, motorMixer_t *customMixers);
 void mixerResetMotors(void);
-float getTiltAngle(void);
+float getTiltServoAngle(void);
 void servoTilting(void);
 void mixTilting(void);
 void mixTable(void);
