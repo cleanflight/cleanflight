5357a4f49700a35c96867e96872c914712c08e76
Mauro Mombelli <mauro@mombelli.me>
2015-05-19 15:29:55 +0200

    Added thrust compensation for body tilt

--- src/main/io/serial_cli.c
+++ src/main/io/serial_cli.c
@@ -843,7 +843,7 @@ static void cliTiltArm(char *cmdline)
         }
 
         printf(" gear ratio: %f",
-            tilt->gearRatio
+            (double)tilt->gearRatio //avoid implicit conversion warning; printf use double
         );
 
         printf("\r\n");
