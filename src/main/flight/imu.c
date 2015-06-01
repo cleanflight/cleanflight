5d894888c82e4f0dd72cb342a0487ac7de6e5e84
Mauro Mombelli <mauro@mombelli.me>
2015-06-01 20:16:25 +0200

    bug fixed, as percent to 1000 range just ned a 10 not a 100

--- src/main/flight/imu.c
+++ src/main/flight/imu.c
@@ -344,6 +344,6 @@ int16_t calculateThrottleAngleCorrection(uint8_t throttle_correction_value)
 }
 
 int16_t calculateThrottleAngleCorrectionAxis(uint8_t throttle_hovering, uint8_t axis){
-    uint16_t liftOffTrust = throttle_hovering * 100;
+    uint16_t liftOffTrust = throttle_hovering * 10;
     return lrintf( liftOffTrust*sinf(anglerad[axis]) );
 }
