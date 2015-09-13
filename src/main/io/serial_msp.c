31eda267d5918a16d535f94b7a55c51cdbb7ea37
Sami Korhonen <samik.korhonen@gmail.com>
2015-09-13 14:45:44 +0300

    MultiFlasher support for SimonK escs
    Conflicts:
    	src/main/drivers/serial_escserial.c
    	src/main/drivers/serial_escserial.h
    	src/main/io/serial_msp.c

--- src/main/io/serial_msp.c
+++ src/main/io/serial_msp.c
@@ -85,6 +85,9 @@
 
 #include "serial_msp.h"
 
+#ifdef USE_ESCSERIAL
+#include "drivers/serial_escserial.h"
+#endif
 static serialPort_t *mspSerialPort;
 
 extern uint16_t cycleTime; // FIXME dependency on mw.c
@@ -308,6 +311,7 @@ static const char * const boardIdentifier = TARGET_BOARD_IDENTIFIER;
 #define MSP_GPSSVINFO            164    //out message         get Signal Strength (only U-Blox)
 #define MSP_SERVO_MIX_RULES      241    //out message         Returns servo mixer configuration
 #define MSP_SET_SERVO_MIX_RULE   242    //in message          Sets servo mixer configuration
+#define MSP_SET_ESCSERIAL        244    //in message          Sets escserial paththrough
 
 #define INBUF_SIZE 64
 
@@ -1730,6 +1734,49 @@ static bool processInCommand(void)
     case MSP_REBOOT:
         isRebootScheduled = true;
         break;
+
+
+#ifdef USE_ESCSERIAL
+    case MSP_SET_ESCSERIAL:
+        // get channel number
+        i = read8();
+        // we do not give any data back, assume channel number is transmitted OK
+        if (i == 0xFF) {
+            // 0xFF -> preinitialize the Passthrough
+            // switch all motor lines HI
+            escSerialInitialize();
+
+            // and come back right afterwards
+            // rem: App: Wait at least appx. 500 ms for BLHeli to jump into
+            // bootloader mode before try to connect any ESC
+        }
+        else {
+            // Check for channel number 1..USABLE_TIMER_CHANNEL_COUNT-1
+            if ((i > 0) && (i < USABLE_TIMER_CHANNEL_COUNT)) {
+                // because we do not come back after calling escEnablePassthrough
+                // proceed with a success reply first
+                headSerialReply(0);
+                tailSerialReply();
+                // wait for all data to send
+                while (!isSerialTransmitBufferEmpty(mspSerialPort)) {
+                    delay(50);
+                }
+                // Start to activate here
+                // motor 1 => index 0
+                escEnablePassthrough(mspSerialPort,i,0); //sk blmode
+                // MPS uart is active again
+            } else {
+                // ESC channel higher than max. allowed
+                // rem: BLHeliSuite will not support more than 8
+                headSerialError(0);
+            }
+            // proceed as usual with MSP commands
+            // and wait to switch to next channel
+            // rem: App needs to call MSP_BOOT to deinitialize Passthrough
+        }
+        break;
+#endif
+
 
     default:
         // we do not know how to handle the (valid) message, indicate error MSP $M!
