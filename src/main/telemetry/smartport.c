f94d43cf08e0a2134dfa820c1819c536db2c882e
Sebastian Guarino <mailing@sguarin.com.ar>
2015-10-22 12:23:53 -0300

    Fix bad assignation of portmode to 0 via SERIAL_UNIDIR

--- src/main/telemetry/smartport.c
+++ src/main/telemetry/smartport.c
@@ -229,11 +229,7 @@ void configureSmartPortTelemetryPort(void)
         return;
     }
 
-    portOptions = SERIAL_NOT_INVERTED | SERIAL_BIDIR;
-
-    if (telemetryConfig->telemetry_smart_port_2wire) {
-        portOptions &= SERIAL_UNIDIR;
-    }
+    portOptions = SERIAL_NOT_INVERTED | (telemetryConfig->telemetry_smart_port_2wire ? SERIAL_UNIDIR : SERIAL_BIDIR);
 
     if (telemetryConfig->telemetry_inversion) {
         portOptions |= SERIAL_INVERTED;
