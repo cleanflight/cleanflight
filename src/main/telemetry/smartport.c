beabf2f0212cfaf0997e8ff0842b3829ef8d5aeb
Your Name <you@example.com>
2016-02-23 10:05:18 +0100

    add airmode support

--- src/main/telemetry/smartport.c
+++ src/main/telemetry/smartport.c
@@ -415,9 +415,7 @@ void handleSmartPortTelemetry(void)
                     tmpi += 10;
                 if (FLIGHT_MODE(HORIZON_MODE))
                     tmpi += 20;
-                if (FLIGHT_MODE(UNUSED_MODE))
-                    tmpi += 40;
-                if (FLIGHT_MODE(PASSTHRU_MODE))
+                if (IS_RC_MODE_ACTIVE(BOXAIRMODE))
                     tmpi += 40;
 
                 if (FLIGHT_MODE(MAG_MODE))
