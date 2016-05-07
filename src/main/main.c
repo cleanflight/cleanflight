241fcb87c81836a4a82c0e7710d96618b26f07e7
Dominic Clifton <dominic.clifton@cleanflight.com>
2016-05-08 01:00:40 +0200

    update beeper code to fix F1 targets, missed in 6c4a07e0dabc1b1356967affe66364b26813e9c1

--- src/main/main.c
+++ src/main/main.c
@@ -279,8 +279,8 @@ void init(void)
 #ifdef NAZE
     if (hardwareRevision >= NAZE32_REV5) {
         // naze rev4 and below used opendrain to PNP for buzzer. Rev5 and above use PP to NPN.
-        beeperConfig.gpioMode = Mode_Out_PP;
-        beeperConfig.isInverted = true;
+        beeperIOConfig.gpioMode = Mode_Out_PP;
+        beeperIOConfig.isInverted = true;
     }
 #endif
 
