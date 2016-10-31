123f70be7efec656a9ddce113a5565fd7527fded
Aravind <aravind4113546@gmail.com>
2016-10-31 17:58:30 -0400

    Checking usbwrite

--- out/obj/bus_i2c_edison.d
+++
@@ -1,6 +0,0 @@
-bus_i2c_edison.o: src/main/drivers/bus_i2c_edison.c src/main/includes.h \
- src/main/drivers/bus_i2c.h
-
-src/main/includes.h:
-
-src/main/drivers/bus_i2c.h:
--- out/obj/serial_uart.d
+++
@@ -1,6 +0,0 @@
-serial_uart.o: src/main/drivers/serial_uart.c \
- src/main/drivers/../includes.h src/main/drivers/serial_uart.h
-
-src/main/drivers/../includes.h:
-
-src/main/drivers/serial_uart.h:
