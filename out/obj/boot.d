a34e947be979aea2a9d900940e8b149118ff7d32
Aravind <aravind4113546@gmail.com>
2016-10-31 15:56:00 -0400

    Corrected compilation issues

---
+++ out/obj/boot.d
@@ -0,0 +1,13 @@
+boot.o: src/main/fc/boot.c src/main/includes.h src/main/drivers/bus_i2c.h \
+ src/main/target/edison/target.h src/main/drivers/system.h \
+ src/main/drivers/serial_uart.h
+
+src/main/includes.h:
+
+src/main/drivers/bus_i2c.h:
+
+src/main/target/edison/target.h:
+
+src/main/drivers/system.h:
+
+src/main/drivers/serial_uart.h:
---
+++ out/obj/system.d
@@ -0,0 +1,3 @@
+system.o: src/main/drivers/system.c src/main/drivers/serial_uart.h
+
+src/main/drivers/serial_uart.h:
