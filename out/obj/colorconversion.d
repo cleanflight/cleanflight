6d16af371087e2403532f1e064ab275522eeec2c
Aravind <aravind4113546@gmail.com>
2016-11-24 13:46:56 -0500

    Intermediate

--- out/obj/colorconversion.d
+++
@@ -1,6 +0,0 @@
-colorconversion.o: src/main/common/colorconversion.c \
- src/main/common/color.h src/main/common/colorconversion.h
-
-src/main/common/color.h:
-
-src/main/common/colorconversion.h:
--- out/obj/maths.d
+++
@@ -1,6 +0,0 @@
-maths.o: src/main/common/maths.c src/main/common/axis.h \
- src/main/common/maths.h
-
-src/main/common/axis.h:
-
-src/main/common/maths.h:
