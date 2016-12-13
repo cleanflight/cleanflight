ba94d95b82920511581c145bf5c532d151415bf4
Andrey Mironov <diehertz@gmail.com>
2016-12-13 20:51:01 +0300

    Update serial_4way.c

--- src/main/io/serial_4way.c
+++ src/main/io/serial_4way.c
@@ -314,7 +314,7 @@ static uint8_t readByteCrc(void)
 
 static void writeByte(uint8_t b)
 {
-    while (!serialTxBytesFree(instance)) {
+    while (!serialTxBytesFree(port)) {
     };
     serialWrite(port, b);
 }
