betaflight/betaflight

commit ee65eba88ddeff054769e8a41e2f1793a712806b

Added selectable RaceFlight rates.

src/main/target/NAZE/config.c
@@ -85,9 +85,11 @@ void targetConfiguration(void)
     for (uint8_t rateProfileIndex = 0; rateProfileIndex < CONTROL_RATE_PROFILE_COUNT; rateProfileIndex++) {
         controlRateConfig_t *controlRateConfig = controlRateProfilesMutable(rateProfileIndex);
 
-        controlRateConfig->rcRate8 = 100;
-        controlRateConfig->rcYawRate8 = 110;
-        controlRateConfig->rcExpo8 = 0;
+        controlRateConfig->rcRates[FD_ROLL] = 100;
+        controlRateConfig->rcRates[FD_PITCH] = 100;
+        controlRateConfig->rcRate[FD_YAW] = 110;
+        controlRateConfig->rcExpo[FD_ROLL] = 0;
+        controlRateConfig->rcExpo[FD_PITCH] = 0;
         controlRateConfig->rates[FD_ROLL] = 77;
         controlRateConfig->rates[FD_PITCH] = 77;
         controlRateConfig->rates[FD_YAW] = 80;

src/main/fc/config.c
@@ -137,7 +137,7 @@ void resetConfigs(void)
 void activateConfig(void)
 {
 #ifndef USE_OSD_SLAVE
-    generateThrottleCurve();
+    initRcProcessing();
 
     resetAdjustmentStates();
 

src/main/target/ALIENWHOOP/config.c
@@ -130,9 +130,11 @@ void targetConfiguration(void)
         controlRateConfig_t *controlRateConfig = controlRateProfilesMutable(rateProfileIndex);
 
         /* RC Rates */
-        controlRateConfig->rcRate8 = 100;
-        controlRateConfig->rcYawRate8 = 100;
-        controlRateConfig->rcExpo8 = 0;
+        controlRateConfig->rcRates[FD_ROLL] = 100;
+        controlRateConfig->rcRates[FD_PITCH] = 100;
+        controlRateConfig->rcRates[FD_YAW] = 100;
+        controlRateConfig->rcExpo[FD_ROLL] = 0;
+        controlRateConfig->rcExpo[FD_PITCH] = 0;
 
         /* Super Expo Rates */
         controlRateConfig->rates[FD_ROLL] = 80;

src/main/target/BEEBRAIN_V2F/config.c
@@ -85,9 +85,10 @@ void targetConfiguration(void)
     for (uint8_t rateProfileIndex = 0; rateProfileIndex < CONTROL_RATE_PROFILE_COUNT; rateProfileIndex++) {
         controlRateConfig_t *controlRateConfig = controlRateProfilesMutable(rateProfileIndex);
 
-        controlRateConfig->rcYawRate8      = 120;
-        controlRateConfig->rcExpo8         = 15;
-        controlRateConfig->rcYawExpo8      = 15;
+        controlRateConfig->rcRates[FD_YAW] = 120;
+        controlRateConfig->rcExpo[FD_ROLL] = 15;
+        controlRateConfig->rcExpo[FD_PITCH] = 15;
+        controlRateConfig->rcExpo[FD_YAW]  = 15;
         controlRateConfig->rates[FD_ROLL]  = 85;
         controlRateConfig->rates[FD_PITCH] = 85;
     }

src/main/target/FF_PIKOBLX/config.c
@@ -80,10 +80,12 @@ void targetConfiguration(void)
         for (uint8_t rateProfileIndex = 0; rateProfileIndex < CONTROL_RATE_PROFILE_COUNT; rateProfileIndex++) {
             controlRateConfig_t *controlRateConfig = controlRateProfilesMutable(rateProfileIndex);
 
-            controlRateConfig->rcRate8 = 100;
-            controlRateConfig->rcYawRate8 = 100;
-            controlRateConfig->rcExpo8 = 15;
-            controlRateConfig->rcYawExpo8 = 15;
+            controlRateConfig->rcRates[FD_ROLL] = 100;
+            controlRateConfig->rcRates[FD_PITCH] = 100;
+            controlRateConfig->rcRates[FD_YAW] = 100;
+            controlRateConfig->rcExpo[FD_ROLL] = 15;
+            controlRateConfig->rcExpo[FD_PITCH] = 15;
+            controlRateConfig->rcExpo[FD_YAW] = 15;
             controlRateConfig->rates[PID_ROLL] = 80;
             controlRateConfig->rates[PID_PITCH] = 80;
             controlRateConfig->rates[PID_YAW] = 80;

src/main/target/MULTIFLITEPICO/config.c
@@ -92,7 +92,8 @@ void targetConfiguration(void)
     for (uint8_t rateProfileIndex = 0; rateProfileIndex < CONTROL_RATE_PROFILE_COUNT; rateProfileIndex++) {
         controlRateConfig_t *controlRateConfig = controlRateProfilesMutable(rateProfileIndex);
 
-        controlRateConfig->rcRate8 = 70;
+        controlRateConfig->rcRates[FD_ROLL] = 70;
+        controlRateConfig->rcRates[FD_PITCH] = 70;
     }
 }
 #endif

src/main/blackbox/blackbox.c
@@ -1232,14 +1232,16 @@ static bool blackboxWriteSysinfo(void)
         BLACKBOX_PRINT_HEADER_LINE("looptime", "%d",                        gyro.targetLooptime);
         BLACKBOX_PRINT_HEADER_LINE("gyro_sync_denom", "%d",                 gyroConfig()->gyro_sync_denom);
         BLACKBOX_PRINT_HEADER_LINE("pid_process_denom", "%d",               pidConfig()->pid_process_denom);
-        BLACKBOX_PRINT_HEADER_LINE("rc_rate", "%d",                         currentControlRateProfile->rcRate8);
-        BLACKBOX_PRINT_HEADER_LINE("rc_expo", "%d",                         currentControlRateProfile->rcExpo8);
-        BLACKBOX_PRINT_HEADER_LINE("rc_rate_yaw", "%d",                     currentControlRateProfile->rcYawRate8);
-        BLACKBOX_PRINT_HEADER_LINE("rc_expo_yaw", "%d",                     currentControlRateProfile->rcYawExpo8);
         BLACKBOX_PRINT_HEADER_LINE("thr_mid", "%d",                         currentControlRateProfile->thrMid8);
         BLACKBOX_PRINT_HEADER_LINE("thr_expo", "%d",                        currentControlRateProfile->thrExpo8);
         BLACKBOX_PRINT_HEADER_LINE("tpa_rate", "%d",                        currentControlRateProfile->dynThrPID);
         BLACKBOX_PRINT_HEADER_LINE("tpa_breakpoint", "%d",                  currentControlRateProfile->tpa_breakpoint);
+        BLACKBOX_PRINT_HEADER_LINE("rc_rates", "%d,%d,%d",                  currentControlRateProfile->rcRates[ROLL],
+                                                                            currentControlRateProfile->rcRates[PITCH],
+                                                                            currentControlRateProfile->rcRates[YAW]);
+        BLACKBOX_PRINT_HEADER_LINE("rc_expo", "%d,%d,%d",                   currentControlRateProfile->rcExpo[ROLL],
+                                                                            currentControlRateProfile->rcExpo[PITCH],
+                                                                            currentControlRateProfile->rcExpo[YAW]);
         BLACKBOX_PRINT_HEADER_LINE("rates", "%d,%d,%d",                     currentControlRateProfile->rates[ROLL],
                                                                             currentControlRateProfile->rates[PITCH],
                                                                             currentControlRateProfile->rates[YAW]);

src/main/cms/cms_menu_imu.c
@@ -203,15 +203,17 @@ static OSD_Entry cmsx_menuRateProfileEntries[] =
 {
     { "-- RATE --", OME_Label, NULL, rateProfileIndexString, 0 },
 
-    { "RC RATE",     OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcRate8,    0, 255, 1, 10 }, 0 },
-    { "RC YAW RATE", OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcYawRate8, 0, 255, 1, 10 }, 0 },
+    { "RC R RATE",   OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcRates[FD_ROLL],    0, 255, 1, 10 }, 0 },
+    { "RC P RATE",   OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcRates[FD_PITCH],    0, 255, 1, 10 }, 0 },
+    { "RC Y RATE",   OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcRates[FD_YAW], 0, 255, 1, 10 }, 0 },
 
-    { "ROLL SUPER",  OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rates[0],   0, 100, 1, 10 }, 0 },
-    { "PITCH SUPER", OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rates[1],   0, 100, 1, 10 }, 0 },
-    { "YAW SUPER",   OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rates[2],   0, 100, 1, 10 }, 0 },
+    { "ROLL SUPER",  OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rates[FD_ROLL],   0, 100, 1, 10 }, 0 },
+    { "PITCH SUPER", OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rates[FD_PITCH],   0, 100, 1, 10 }, 0 },
+    { "YAW SUPER",   OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rates[FD_YAW],   0, 100, 1, 10 }, 0 },
 
-    { "RC EXPO",     OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcExpo8,    0, 100, 1, 10 }, 0 },
-    { "RC YAW EXP",  OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcYawExpo8, 0, 100, 1, 10 }, 0 },
+    { "RC R EXPO",   OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcExpo[FD_ROLL],    0, 100, 1, 10 }, 0 },
+    { "RC P EXPO",   OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcExpo[FD_PITCH],    0, 100, 1, 10 }, 0 },
+    { "RC Y EXPO",   OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &rateProfile.rcExpo[FD_YAW], 0, 100, 1, 10 }, 0 },
 
     { "THR MID",     OME_UINT8,  NULL, &(OSD_UINT8_t) { &rateProfile.thrMid8,           0,  100,  1}, 0 },
     { "THR EXPO",    OME_UINT8,  NULL, &(OSD_UINT8_t) { &rateProfile.thrExpo8,          0,  100,  1}, 0 },

cleanflight/cleanflight

commit 3669650b7d0c73c6a11641fc745b9cae376aea3d

Fix BEEBRAIN compilation.
Broken since ee65eba88ddeff054769e8a41e2f1793a712806b

src/main/target/NAZE/config.c
@@ -91,7 +91,7 @@ void targetConfiguration(void)
 
         controlRateConfig->rcRates[FD_ROLL] = 100;
         controlRateConfig->rcRates[FD_PITCH] = 100;
-        controlRateConfig->rcRate[FD_YAW] = 110;
+        controlRateConfig->rcRates[FD_YAW] = 110;
         controlRateConfig->rcExpo[FD_ROLL] = 0;
         controlRateConfig->rcExpo[FD_PITCH] = 0;
         controlRateConfig->rates[FD_ROLL] = 77;