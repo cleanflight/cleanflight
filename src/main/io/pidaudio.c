564d96ca1e2b5baea609661771bb5f497f2b5ab1
Hydra <dominic.clifton@cleanflight.com>
2018-03-11 19:57:18 +0100

    CF/BF - Refactor initial cut of PID-Audio code into driver level and high-level code.

--- src/main/io/pidaudio.c
+++ src/main/io/pidaudio.c
@@ -17,95 +17,34 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <string.h>
 #include <math.h>
 
 #include <platform.h>
 
-#include "build/build_config.h"
-#include "build/debug.h"
-
 #include "common/axis.h"
 #include "common/maths.h"
-#include "common/filter.h"
 
-#include "config/config_reset.h"
-#include "pg/pg.h"
-#include "pg/pg_ids.h"
+#include "drivers/audio.h"
 
-//#include "drivers/sound_beeper.h"
-//#include "drivers/time.h"
-
-#include "fc/fc_core.h"
 #include "fc/rc_modes.h"
-#include "fc/fc_rc.h"
-
-#include "fc/rc_controls.h"
-#include "fc/runtime_config.h"
 
 #include "flight/pid.h"
-#include "flight/imu.h"
-#include "flight/mixer.h"
-
-#include "io/gps.h"
-
-#include "sensors/gyro.h"
-#include "sensors/acceleration.h"
-
-static DAC_HandleTypeDef hdac;
-static TIM_HandleTypeDef handle;
-static DAC_ChannelConfTypeDef sConfig;
 
 static bool pidAudioEnabled = false;
 
 void pidAudioInit(void)
 {
-    __HAL_RCC_DAC_CLK_ENABLE();
-    __HAL_RCC_TIM6_CLK_ENABLE();
-
-    hdac.Instance = DAC;
-    HAL_DAC_Init(&hdac);
-
-    GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.Pin = GPIO_PIN_4;
-    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    audioSetupIO();
 }
 
 void pidAudioStart(void)
 {
-
-    handle.Instance = TIM6;
-    handle.Init.Period = 0xFF; // 42Mhz / 256 = 16.406khz
-    handle.Init.Prescaler = 0;
-
-    handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-    handle.Init.CounterMode = TIM_COUNTERMODE_UP;
-    handle.Init.RepetitionCounter = 0;
-    HAL_TIM_Base_Init(&handle);
-
-    TIM_MasterConfigTypeDef sMasterConfig;
-    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_ENABLE;
-    sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
-    HAL_TIMEx_MasterConfigSynchronization(&handle, &sMasterConfig);
-
-    HAL_TIM_Base_Start(&handle);
-
-    sConfig.DAC_Trigger = DAC_TRIGGER_T6_TRGO;
-    sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
-    HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_1);
-
-    HAL_DACEx_NoiseWaveGenerate(&hdac, DAC_CHANNEL_1, DAC_LFSRUNMASK_BITS10_0);
-    HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_L, 0xcd00);
-
-    HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
+    audioGenerateWhiteNoise();
 }
 
 void pidAudioStop(void)
 {
-    HAL_DAC_Stop(&hdac, DAC_CHANNEL_1);
-    HAL_TIM_Base_Stop(&handle);
+    audioSilence();
 }
 
 void pidAudioUpdate(void)
@@ -127,7 +66,6 @@ void pidAudioUpdate(void)
 
     const uint32_t pidSumXY = MIN((ABS(axisPIDSum[FD_ROLL]) + ABS(axisPIDSum[FD_YAW])) / 2, PIDSUM_LIMIT);
 
-    uint32_t newPeriod = scaleRange(pidSumXY, 0, PIDSUM_LIMIT, 64, 576);
-    handle.Init.Period = newPeriod;
-    TIM_Base_SetConfig(handle.Instance, &handle.Init);
+    uint32_t tone = scaleRange(pidSumXY, 0, PIDSUM_LIMIT, TONE_MIN, TONE_MAX);
+    audioPlayTone(tone);
 }
