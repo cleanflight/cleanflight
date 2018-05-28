b8040b6038f9b1dbd71f4765e5d3b3f7798545ef
Dominic Clifton <dominic.clifton@cleanflight.com>
2018-05-28 17:56:55 +0200

    CF/BF - Re-enable BlHeli 4way on Naze32.
    Older FC's are most likely to have older ESC's that need upgrading.

--- src/main/target/NAZE/target.h
+++ src/main/target/NAZE/target.h
@@ -148,6 +148,8 @@
 
 #define DEFAULT_RX_FEATURE      FEATURE_RX_PPM
 
+#define USE_SERIAL_4WAY_BLHELI_INTERFACE
+
 // IO - assuming all IOs on 48pin package
 #define TARGET_IO_PORTA         0xffff
 #define TARGET_IO_PORTB         0xffff
