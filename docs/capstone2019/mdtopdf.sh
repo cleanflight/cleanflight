1d05d595bedb54d9042a9c78dc79aaf93271f603
nnikolov3 <nnikolov@pdx.edu>
2020-01-26 22:42:20 -0800

    Progress Reports week 1,2,3 done

--- docs/capstone2019/mdtopdf.sh
+++ docs/capstone2019/mdtopdf.sh
@@ -4,7 +4,14 @@ for f in **/*.md;
 
 do
 	filename="${f%.*}"	
-	pandoc $f -V geometry:margin=1in --latex-engine=lualatex -o $filename.pdf
+	pandoc  $f --from=markdown \
+		-V linkcolor:blue \
+		-V geometry:a4paper \
+		-V geometry:margin=2cm \
+		-V mainfont="DejaVu Serif" \
+		-V monofont="DejaVu Sans Mono" \
+		--latex-engine=lualatex \
+		-o $filename.pdf	
 done
 
 
